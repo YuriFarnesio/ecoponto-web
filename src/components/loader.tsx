8315892afa5708b1ba2527a2ec8be49f7fb79721
Yuri Farnesio <yuri.farnesio@gmail.com>
2024-10-26 15:18:46 -0300

    fix: remove commented lines

--- src/components/loader.tsx
+++ src/components/loader.tsx
@@ -6,8 +6,6 @@ export function Loader({ message = 'Carregando dados...' }: LoaderProps) {
   return (
     <div className="flex flex-col items-center justify-center absolute inset-0 gap-4 bg-overlay z-50 px-4">
       <img
-        // width={96}
-        // height={88}
         src="/ecoponto.svg"
         alt="Logo EcoPonto"
         className="w-14 h-12 md:w-24 md:h-[5.5rem] pointer-events-none animate-bounce"
